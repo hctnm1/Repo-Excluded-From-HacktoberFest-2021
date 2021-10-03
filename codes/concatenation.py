dd5752c1080246f6b111f14c45b08517061b36cc
jyoti-anjaly <jyotisingh7782019@gmail.com>
2021-10-03 14:04:49 +0530

    Concatenation of two strings

---
+++ codes/concatenation.py
@@ -0,0 +1,9 @@
+#Concatenation of two strings 
+
+# Defining strings
+variable1 = "Welcome to "
+variable2 = "Hacktober fest"
+ 
+# + Operator is used to combine strings
+variable3 = variable1 + variable2
+print(variable3)
